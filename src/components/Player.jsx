9cae8b72c4f370af1ef4bdfeb6ee7da41233830e
Dorian Kita <dorian.kita90@gmail.com>
2024-02-07 18:22:53 +0100

    used useRef hook for handling adding name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const inputValue = useRef();
+
+  const [name, setName] = useState();
+
+  function handleNameChange() {
+    setName(inputValue.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputValue} />
+        <button onClick={handleNameChange}>Set Name</button>
       </p>
     </section>
   );
